0904cf9a2a6cae9a77e10ef673b599d4e40372ca
Ariel Levy <ariel.levi@motorolasolutions.com>
2018-03-07 11:30:54 +0200

    add func2. version 1.1

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 
-#define VERSION 1.0
+#define VERSION 1.1
 
 int main()
 {
    prinf("ProjectX version %s\n", VERSION);
-   func(3);
+   func1(3);
+   func2(3);
    return 0;
 }
 
 
 int func(int x)
 {
-   prinf("got number $d\n", x);
+   prinf("func1: got number $d\n", x);
+}
+
+int func2(int x)
+{
+   prinf("func2: got number $d\n", x);
 }
