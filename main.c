c369912da2c719074c59944a3a5a8b1e2808197e
Ariel Levy <ariel.levi@motorolasolutions.com>
2018-03-07 11:41:15 +0200

    version 1.2

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
-#define VERSION 1.1
+#define VERSION 1.2
 
 int main()
 {
    prinf("ProjectX version %s\n", VERSION);
    func1(3);
-   func2(3);
+   func2(5);
    return 0;
 }
 
